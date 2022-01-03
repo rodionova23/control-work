7356d2cee6b20711ff944c06a12110b7a5979cd5
Olga Rodionova <lelya_23@mail.ru>
2022-01-03 15:39:53 +0300

    Вывод первоначального массива

--- Program.cs
+++ Program.cs
@@ -3,3 +3,9 @@ int n =int.Parse(Console.ReadLine());
 int [] arr = new int[n];  
 int count=0;
 Random rand = new Random();
+Console.WriteLine("исходная последовательность:");
+for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand.Next(1, 100);     
+        Console.Write(arr[i] + " ");
+    }
