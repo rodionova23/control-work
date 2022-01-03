9359997e65182c61f02fa0a219479f0e295d8bfb
Olga Rodionova <lelya_23@mail.ru>
2022-01-03 15:34:20 +0300

    задана длина массива

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите колличество элементов в последовательности:");
+int n =int.Parse(Console.ReadLine());
+int [] arr = new int[n];  
+int count=0;
+Random rand = new Random();
